0dd0705cb94411b303243e09f37cb6349747a18b
MartaKoss27 <27691497m@gmail.com>
2023-05-30 20:48:33 +0200

    Update task 05 js

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+//spr.1
+console.log(inputName.value);
+console.log(outputName.textContent);
+
+//function updateName() {
+//if (inputName.value !== "") {
+//  inputName.value = outputName.textContent;
+//} else {
+// outputName.textContent = "Anonymous";
+//}
+//}
+
+// inputName.addEventListener("input", updateName);
