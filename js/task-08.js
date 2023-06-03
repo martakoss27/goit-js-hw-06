2c579e80bb0e7962276b3d2437581b2f44949d71
MartaKoss27 <27691497m@gmail.com>
2023-06-03 20:14:30 +0200

    task 08 js done

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,22 @@
 const form = document.querySelector(".login-form");
 console.log(form);
-//If the form has empty fields,
-//display alert saying that all fields must be filled in.
-function validateForm() {
-  if (form.email.value === "" || form.password.value === "") {
-    alert("Please Fill In All Required Fields");
-    return;
-  }
-}
-//As soon as the user has filled in all the fields and submitted the form,
-//collect the field values into an object,
-//where the field name will be the property name and the field value will be the property value.
-// Use the elements property to access form elements.
-form.addEventListener("submit", (event) => {
+
+const submitData = (event) => {
   event.preventDefault();
-  validateForm;
+
   const {
     elements: { email, password },
   } = event.currentTarget;
   console.log(email.value, password.value);
-});
+
+  if (!email.value || !password.value) {
+    window.alert("Please Fill In All Required Fields");
+  } else {
+    const loginData = {};
+    loginData[email.name] = email.value;
+    loginData[password.name] = password.value;
+    console.log(loginData);
+    event.currentTarget.reset();
+  }
+};
+form.addEventListener("submit", submitData);
