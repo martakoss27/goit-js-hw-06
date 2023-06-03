5ca4632ab27fcf9b4b1f55c89915dcf5f9280957
MartaKoss27 <27691497m@gmail.com>
2023-06-03 15:50:49 +0200

    update task 09 js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,7 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const color = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", (event) => {});
