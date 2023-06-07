9376bedfc6a199999ff24625bcd597a0502106d1
MartaKoss27 <27691497m@gmail.com>
2023-06-07 14:13:49 +0200

    deleting unnecessary console logs

--- js/task-05.js
+++ js/task-05.js
@@ -1,10 +1,6 @@
 const inputName = document.querySelector("#name-input");
 const outputName = document.querySelector("#name-output");
 
-//spr.1
-console.log(inputName.value);
-console.log(outputName.textContent);
-
 const nameInputEvent = (event) => {
   const inputValue = event.currentTarget;
   outputName.textContent = inputValue.value ? inputValue.value : "Anonymous";
--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,4 @@
 const form = document.querySelector(".login-form");
-console.log(form);
 
 const submitData = (event) => {
   event.preventDefault();
