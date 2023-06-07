fc1c2b393a914c9a6503eaba9bf8fb4a1ff59995
MartaKoss27 <27691497m@gmail.com>
2023-06-07 14:20:24 +0200

    removed unneccesary comments

--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,7 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-//Write a script that, for each element in the ingredients array:
 
-//Will create a separate <li> element. Be sure to use the document.createElement() method.
-//Will add the ingredient name as its text content.
-//Will add the item class to the element.
-//Then will insert all <li> to the ul#ingredients list in a single operation.
 const itemsList = document.querySelector("#ingredients");
 
 ingredients.forEach((element) => {
